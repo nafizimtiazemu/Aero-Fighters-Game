75715268150e55ac359078125bebc6d1021ea54b
nafizimtiazemu <nafisemu007@gmail.com>
2025-11-25 04:10:44 -0500

    Added Bullet and Enemy class

---
+++ src/Game/Bullet.java
@@ -0,0 +1,30 @@
+package Game;
+
+import java.awt.*;
+
+public class Bullet {
+
+    public int x, y;
+    public int width = 5;
+    public int height = 12;
+    public int speed = 10;
+
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update() {
+        y -= speed;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, width, height);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+
+}
---
+++ src/Game/Enemy.java
@@ -0,0 +1,34 @@
+package Game;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Enemy{
+    public int x, y;
+    public int width = 40;
+    public int height = 40;
+    public int speed;
+
+    public Enemy(int panelWidth) {
+        Random rand = new Random();
+
+        this.x = rand.nextInt(panelWidth - width);
+
+        this.y = -height;
+
+        this.speed = 2 + rand.nextInt(4);
+    }
+
+    public void update() {
+        y += speed;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, width, height);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
